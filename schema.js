a56dbc8d3fe288094863f2ae3e759293bb4bd2b8
Milo MacPhail <milomacphail@gmail.com>
2018-10-29 22:44:44 +0000

    Fixed typo

--- schema.js
+++ schema.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const portfolioSchema = new Schema({
+    name: String,
     ticker: String,
     stockRank: Number,
-    breed: String,
+    type: String,
     price: Number,
     shares: Number,
     purchaseDate: { type: Date, default: Date.now },
     competitorStocks: [{String, Number}]
+    
 });
